72d2f69341965aa276fd63d49f5e3c3be3bc66b5
almaaprl <162028579+almaaprl@users.noreply.github.com>
2024-09-24 22:15:20 +0700

    Update Kalkulator.java

--- Pertemuan 4/Kalkulator.java
+++ Pertemuan 4/Kalkulator.java
@@ -1,1 +1,29 @@
-//code
+public class Kalkulator
+{
+    private double num1;
+    private double num2;
+
+    public void isiNum1 (double x){
+        num1 = x;
+    }
+    
+    public void isiNum2 (double y) {
+        num2 = y;
+    }
+    
+    public double tambah(double num1, double num2){
+        return num1 + num2;
+    }
+    
+    public double kurang(double num1, double num2){
+        return num1 - num2;
+    }
+    
+    public double kali(double num1, double num2){
+        return num1 * num2;
+    }
+    
+    public double bagi(double num1, double num2){
+        return num1 / num2;
+    }
+}
