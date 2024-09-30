28772ff9cb8e66c5046f1fa261065c64ca9eebe9
almaaprl <162028579+almaaprl@users.noreply.github.com>
2024-09-30 12:58:39 +0700

    Create Bid.java

---
+++ Pertemuan 5/Bid.java
@@ -0,0 +1,17 @@
+public class Bid {
+    private Person bidder;
+    private long value;
+
+    public Bid(Person bidder, long value) {
+        this.bidder = bidder;
+        this.value = value;
+    }
+
+    public Person getBidder() {
+        return bidder;
+    }
+
+    public long getValue() {
+        return value;
+    }
+}
